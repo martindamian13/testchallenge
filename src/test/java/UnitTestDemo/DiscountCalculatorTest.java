b6968655859cad3c3b879e4264a8d907a71bbf78
Agustin Arce <agustinarceprincich@gmail.com>
2024-10-15 17:04:31 -0300

    test: add test case 01, 03 and 04

--- src/test/java/UnitTestDemo/DiscountCalculatorTest.java
+++ src/test/java/UnitTestDemo/DiscountCalculatorTest.java
@@ -1,7 +1,32 @@
 package UnitTestDemo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import UnitTestDemo.DiscountCalculator.MembershipLevel;
 
 class DiscountCalculatorTest {
+    DiscountCalculator calculator = new DiscountCalculator();
 
+    @Test()
+    void testCaseOne() {
+        double want = 100.0;
+        double got = calculator.calculateFinalPrice(100.0, MembershipLevel.NONE);
+        assertEquals(want, got);
+    }
+
+    @Test()
+    void testCaseThree() {
+        double want = 100.0;
+        double got = calculator.calculateFinalPrice(100.0, MembershipLevel.SILVER);
+        assertEquals(want, got);
+    }
+
+    @Test()
+    void testCaseFour() {
+        double want = 95.94999999999999;
+        double got = calculator.calculateFinalPrice(101.0, MembershipLevel.SILVER);
+        assertEquals(want, got);
+    }
 }
